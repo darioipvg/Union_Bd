b8244f3d3594e1c3aa9ee93a0074895134590b1a
Darío Irribarra <dario.irribarra.b@gmail.com>
2021-04-30 20:30:27 -0400

    1.- Creación de controler "Marcas" 2.- Guardado en bd 3.- Validación en el controlador al pasarle la clase

---
+++ inventario/Controllers/MarcasController.cs
@@ -0,0 +1,43 @@
+﻿using inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inventario.Controllers
+{
+    public class MarcasController : Controller
+    {
+        //Se instancia la clase como conexión a la bd
+        private inventariodbEntities db = new inventariodbEntities();
+
+        // GET: Marcas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //Se le pasa la clase completamente
+        public ActionResult Create(Marca marca)
+        {
+
+            //Validaciones
+            if (ModelState.IsValid)
+            {
+                //Guardamos en base de datos
+                db.Marca.Add(marca); //esto es lo mismo que un insert en sql
+                db.SaveChanges();
+            }
+
+            //Se reenvian los datos para que recaegue con los datos
+            return View(marca);
+        }
+    }
+}
